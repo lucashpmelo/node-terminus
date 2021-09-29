537cd2549b7b77bc120d516e96f93058b0a4e8c6
Lucas Melo <lucashpm90@gmail.com>
2021-09-28 23:27:29 -0300

    .

--- src/repositories/nerdcast-repository.js
+++ src/repositories/nerdcast-repository.js
@@ -7,21 +7,13 @@ exports.getConvidadosPorCategoria = async () => {
     { $unwind: "$guests" },
     {
       $group: {
-        _id: "$product",
-        product_name: { $max: "$product_name" },
-        guests: { $push: { id: "$guests.id", name: "$guests.name" } },
-      },
-    },
-    { $unwind: "$guests" },
-    {
-      $group: {
-        _id: { product: "$_id", id: "$guests.id" },
+        _id: { product: "$product", id: "$guests.id" },
         product_name: { $max: "$product_name" },
         nome: { $max: "$guests.name" },
         soma: { $sum: 1 },
       },
     },
-    { $sort: { product_name: 1, soma: -1 } },
+    { $sort: { product_name: 1, soma: -1, nome: 1 } },
     {
       $project: {
         _id: false,
